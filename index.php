32e49a9c06272fa8d42f54fba326f6e8b55bd71f
PiSoftware <42990010+PiSoftware@users.noreply.github.com>
2019-11-17 01:14:22 -0600

    Update #1
    Incluyo el primer cambio al código general del index, el cual no contiene nada de css

--- index.php
+++ index.php
@@ -1,1 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, intial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>CDM Ministerios Ebenezer - Puerto Barrios</title>
+	<link rel="shortcut icon" type="icon/x-icon" href="img/logo.png">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script type="text/javascript" src="scripts/main.js"></script>
+</head>
+<body>
+	<!-- Inicia bloque de portada -->
+		<!-- Inicia bloque de menu de inicio -->
+			<div id="logomenu">
+				<img src="img/logo.png">
+			</div>
+			<div id="menup" class="menu">
+				<nav>
+					<ul>
+						<li><a href="#bienvenidos">Sobre Nosotros</a></li>
+						<li><a href="#servicios">Servicios</a></li>
+						<li><a href="#envivo">En vivo</a></li>
+						<li><a href="#predicas">Predicas</a></li>
+						<li><a href="#actividades">Actividades</a></li>
+						<li><a href="#contacto">Contacto</a></li>
+					</ul>
+				</nav>
+			</div>
+		<!-- Finaliza bloque de menu de inicio -->
+		<img src="img/portadaNOV.jpg">
+	<!-- Finaliza bloque de portada -->
 
+	<!-- Inicia bloque de bienvenidos -->
+		<div id="bienvenidos">
+			<div id="imgbienvenidos">
+				<img src="img/pastor.png">
+			</div>
+			<div id="textobienvenidos">
+				<h2>¡Bienvenido a CDM Ministerios Ebenezer!</h2>
+				<p>Somos una comunidad de personas que se esfuerzan por descubrir el propósito y el plan de Dios para nuestras vidas. Te invitamos a formar parte de nuestra comunidad y vivir la palabra de El Señor.</p>
+			</div>
+		</div>
+	<!-- Finaliza bloque de bienvenidos -->
+
+	<!-- Inicia bloque de servicios -->
+		<div id="servicios">
+			<div id="carrousel">
+				<ul>
+					<li>Img 1</li>
+					<li>Img 2</li>
+					<li>Img 3</li>
+					<li>Img 4</li>
+				</ul>
+			</div>
+		</div>
+	<!-- Finaliza bloque de servicios -->
+
+	<!-- Inicia bloque de envivo -->
+		<div id="envivo">
+			<h1>En vivo</h1>
+			<!-- plugin de en vivo -->
+		</div>
+	<!-- Finaliza bloque de envivo -->
+
+	<!-- Inicia bloque de predicas -->
+		<div id="predicas">
+			<h1>Predicas</h1>
+			<!-- plugin de carrousel de youtube -->
+		</div>
+	<!-- Finaliza bloque de predicas -->
+
+	<!-- Inicia bloque de actividades -->
+		<div id="actividades">
+			<h1>Próximas actividades</h1>
+			<!-- plugin de contador -->
+		</div>
+	<!-- Finaliza bloque de actividades -->
+
+	<!-- Inicia bloque de contacto -->
+		<div id="contacto">
+			<div id="mapa">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15362.103366428473!2d-88.5982642!3d15.7232977!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa3c123be52d507cd!2sCDM%20Ministerios%20Ebenezer!5e0!3m2!1ses!2sgt!4v1573973822257!5m2!1ses!2sgt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+			</div>
+			<div id="formularioc">
+				<form>
+					<h1>Contactanos</h1>
+					<h4>¿Tienes alguna petición o duda? Escríbenos</h4>
+					<div>
+						<input type="text" name="nombre" id="nombre" placeholder="Nombre completo" required="true">
+					</div>
+					<div>
+						<input type="email" name="correo" id="correo" placeholder="Correo electronico" required="true">
+					</div>
+					<div>
+						<input type="text" name="asunto" id="asunto" placeholder="Asunto" required="true">
+					</div>
+					<div>
+						<input type="text" name="telefono" id="telefono" placeholder="Nombre completo">
+					</div>
+					<div>
+						<textarea rows="7" cols="5" placeholder="Escribe tu mensaje aqui"></textarea>
+					</div>
+					<div>
+						<input type="submit" name="enviar" id="enviar" value="Enviar">
+					</div>
+				</form>
+			</div>
+		</div>
+	<!-- Finaliza bloque de contacto -->
+
+	<!-- Inicia bloque de footer -->
+		<!-- Inicia bloque de redes sociales -->
+			<div id="redes">
+				<div id="facebook">
+					<img src="img/facebook.png">
+				</div>
+				<div id="instagram">
+					<img src="img/instagram.png">
+				</div>
+				<div id="youtube">
+					<img src="img/youtube.png">
+				</div>
+				<div id="twitter">
+					<img src="img/twitter.png">
+				</div>
+			</div>
+		<!-- Finaliza bloque de redes sociales -->
+		<footer>
+			<div id="footer">
+				<p>2019-2020 © Casa de Misericordia Ministerios Ebenezer Puerto Barrios | TODOS LOS DERECHOS RESERVADOS</p>
+			</div>
+		</footer>
+	<!-- Finaliza bloque de footer -->
+</body>
+</html>
