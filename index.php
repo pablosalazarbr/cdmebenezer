37401402025c0af20181f454039ada25debb1e19
PiSoftware <42990010+PiSoftware@users.noreply.github.com>
2019-11-29 11:02:41 -0600

    Update Alfa
    Subimos actualización con primer prototipo funcional de la página web

--- index.php
+++ index.php
@@ -7,128 +7,202 @@
 	<title>CDM Ministerios Ebenezer - Puerto Barrios</title>
 	<link rel="shortcut icon" type="icon/x-icon" href="img/logo.png">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<script src="scripts/jquery-3.1.0.min.js"></script>
+	<script src="scripts/youtubefeed.js"></script>
+	<script src="scripts/youtubefeed.min.js"></script>
 	<script type="text/javascript" src="scripts/main.js"></script>
+	<script src='scripts/countdown.js'></script>
 </head>
 <body>
+	<div class="contenedor">
 	<!-- Inicia bloque de portada -->
-		<!-- Inicia bloque de menu de inicio -->
-			<div id="logomenu">
-				<img src="img/logo.png">
-			</div>
-			<div id="menup" class="menu">
-				<nav>
-					<ul>
-						<li><a href="#bienvenidos">Sobre Nosotros</a></li>
-						<li><a href="#servicios">Servicios</a></li>
-						<li><a href="#envivo">En vivo</a></li>
-						<li><a href="#predicas">Predicas</a></li>
-						<li><a href="#actividades">Actividades</a></li>
-						<li><a href="#contacto">Contacto</a></li>
-					</ul>
-				</nav>
-			</div>
-		<!-- Finaliza bloque de menu de inicio -->
-		<img src="img/portadaNOV.jpg">
+
+			<!-- Inicia bloque de menu de inicio -->
+
+				<div id="logomenu" class="logo">
+					<img src="img/logo.png" width="100px" height="100px">
+				</div>
+
+
+					<div id="menup" class="menu">
+						<nav>
+							<ul id="menup">
+								<li><a href="#bienvenidos">Sobre Nosotros</a></li>
+								<li><a href="#servicios">Servicios</a></li>
+								<li><a href="#envivo">En vivo</a></li>
+								<li><a href="#predicas">Prédicas</a></li>
+								<li><a href="#actividades">Actividades</a></li>
+								<li><a href="#contacto">Contacto</a></li>
+							</ul>
+						</nav>
+					</div>
+
+			<!-- Finaliza bloque de menu de inicio -->
+
+				<div id="imgp" class="portada">
+					<img src="img/final-final.jpg" width="100%" alt="Congreso Vision">
+				</div>
+
 	<!-- Finaliza bloque de portada -->
 
 	<!-- Inicia bloque de bienvenidos -->
-		<div id="bienvenidos">
-			<div id="imgbienvenidos">
-				<img src="img/pastor.png">
+		<div id="bienvenidos" class="cbien">
+
+			<div id="imgbienvenidos" class="bienvenido">
+				<img src="img/pastor.png" id="imgpas" width="100%">
 			</div>
-			<div id="textobienvenidos">
-				<h2>¡Bienvenido a CDM Ministerios Ebenezer!</h2>
-				<p>Somos una comunidad de personas que se esfuerzan por descubrir el propósito y el plan de Dios para nuestras vidas. Te invitamos a formar parte de nuestra comunidad y vivir la palabra de El Señor.</p>
+
+			<div id="textobienvenidos" class="textbienvenido"><br>
+				<h2 class="text-center">¡Bienvenido a CDM Ministerios Ebenezer!</h2><br>
+				<p class="text-justify"> <h2 class="text-center">Visión</h2><br> Anhelamos un ministerio  que alcanza y restaura personas y familias, para hacer de ellas vidas íntegras y prósperas, Llenando sus vidas y corazones con la palabra y espíritu del señor y así puedan ver el fruto que nuestro buen Dios y señor Jesucristo espera, contribuyendo a equipar a los santos Y para la edificación del cuerpo de Cristo en amor<br>
+					<br><h2 class="text-center">Misión</h2> <br> un ministerio que alcance equipar a través del espíritu Santo a cada creyente haciendo de él un discípulo eficaz, por medio de las enseñanzas apostólicas y pastorales, Para así conformar a familias integradas que puedan servir al señor; Dando de gracia lo que desgracia hemos recibido: su misericordia <br>
+					<br>Queremos darte una cordial bienvenida y agradecerte por estar en esta casa, y glorificar el nombre de nuestro señor Jesucristo, unánimes juntos al unísono</p>
 			</div>
+
+			<div id="textoCEL" class="textbienvenido">
+					<h2 class="text-center ">¡Bienvenido a CDM Ministerios Ebenezer!</h2><br/>
+					<p class="text-justify">Anhelamos un ministerio  que alcanza y restaura personas y familias, para hacer de ellas vidas íntegras y prósperas, Llenando sus vidas y corazones con la palabra y espíritu del señor y así puedan ver el fruto que nuestro buen Dios y señor Jesucristo espera, contribuyendo a equipar a los santos Y para la edificación del cuerpo de Cristo en amor</p>
+			</div>
+
 		</div>
 	<!-- Finaliza bloque de bienvenidos -->
 
 	<!-- Inicia bloque de servicios -->
-		<div id="servicios">
-			<div id="carrousel">
-				<ul>
-					<li>Img 1</li>
-					<li>Img 2</li>
-					<li>Img 3</li>
-					<li>Img 4</li>
+		<div id="servicios" class="servicio">
+			<div class="slideshow">
+				<ul class="slider">
+					<li>
+						<img src="img/1.jpg" alt="">
+					</li>
+					<li>
+						<img src="img/2.jpg" alt="">
+					</li>
+					<li>
+						<img src="img/3.jpg" alt="">
+					</li>
+					<li>
+						<img src="img/4.jpg" alt="">
+					</li>
 				</ul>
+
+				<div class="left">
+					<span class="fa fa-chevron-left"></span>
+				</div>
+
+				<div class="right">
+					<span class="fa fa-chevron-right"></span>
+				</div>
 			</div>
 		</div>
 	<!-- Finaliza bloque de servicios -->
 
 	<!-- Inicia bloque de envivo -->
-		<div id="envivo">
-			<h1>En vivo</h1>
+		<div id="envivo" class="vivo text-center">
+			<div id="titulovivo" class="titulovivo">
+				<h1 class="titulovivo blanco">En vivo</h1>
+			</div>
 			<!-- plugin de en vivo -->
+			<div id="pluginvivo" class="text-center">
+				<!-- Porfavor, reemplazar la porción de código correspondiente a video en vivo -->
+				<iframe class="espaciobajo frameC" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FCDMEbenezer%2Fvideos%2F3466340076710285%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+			</div>
 		</div>
+
+
 	<!-- Finaliza bloque de envivo -->
 
 	<!-- Inicia bloque de predicas -->
-		<div id="predicas">
-			<h1>Predicas</h1>
+		<div id="predicas" class="predica">
+			<h1 class="titulo1">Predicas</h1>
 			<!-- plugin de carrousel de youtube -->
+			<p>Plugin de Youtube de todas las predicas</p>
 		</div>
 	<!-- Finaliza bloque de predicas -->
 
 	<!-- Inicia bloque de actividades -->
-		<div id="actividades">
-			<h1>Próximas actividades</h1>
+		<div id="actividades" class="actividades">
+			<h1 class="titulo1 blanco" id="tituloa">Próximas actividades</h1><br/>
 			<!-- plugin de contador -->
+			<div id="count-tit">
+				<h1 class="count-tit">Congreso de Jóvenes "Renovando tu visión"</h1>
+			</div>
+			<div id="del-countdown">
+        <div id="clock"></div>
+        <div id="units">
+            <span>Días</span>
+            <span>Horas</span>
+            <span>Minutos</span>
+            <span>Segundos</span>
+        </div>
+    </div>
 		</div>
 	<!-- Finaliza bloque de actividades -->
 
 	<!-- Inicia bloque de contacto -->
-		<div id="contacto">
-			<div id="mapa">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15362.103366428473!2d-88.5982642!3d15.7232977!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa3c123be52d507cd!2sCDM%20Ministerios%20Ebenezer!5e0!3m2!1ses!2sgt!4v1573973822257!5m2!1ses!2sgt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-			</div>
-			<div id="formularioc">
+		<div id="contacto" class="contacto">
+				<div class="mapa">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15362.103366428473!2d-88.5982642!3d15.7232977!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa3c123be52d507cd!2sCDM%20Ministerios%20Ebenezer!5e0!3m2!1ses!2sgt!4v1573973822257!5m2!1ses!2sgt" width="100%" height="500px" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+				</div>
+
+
+
+			<div id="formularioc" class="formulario">
 				<form>
-					<h1>Contactanos</h1>
+					<h1 class="titulo1">Contactanos</h1><br>
+
 					<h4>¿Tienes alguna petición o duda? Escríbenos</h4>
-					<div>
+					<div><br>
 						<input type="text" name="nombre" id="nombre" placeholder="Nombre completo" required="true">
-					</div>
+					</div></br>
 					<div>
 						<input type="email" name="correo" id="correo" placeholder="Correo electronico" required="true">
-					</div>
+					</div></br>
 					<div>
 						<input type="text" name="asunto" id="asunto" placeholder="Asunto" required="true">
-					</div>
+					</div></br>
 					<div>
-						<input type="text" name="telefono" id="telefono" placeholder="Nombre completo">
-					</div>
+						<input type="text" name="telefono" id="telefono" placeholder="Telefono">
+					</div></br>
 					<div>
-						<textarea rows="7" cols="5" placeholder="Escribe tu mensaje aqui"></textarea>
-					</div>
+						<textarea rows="7" name="mensaje" cols="5" placeholder="Escribe tu mensaje aqui"></textarea>
+					</div></br>
 					<div>
 						<input type="submit" name="enviar" id="enviar" value="Enviar">
-					</div>
+					</div></br>
 				</form>
 			</div>
 		</div>
 	<!-- Finaliza bloque de contacto -->
-
+</div>
 	<!-- Inicia bloque de footer -->
 		<!-- Inicia bloque de redes sociales -->
-			<div id="redes">
+			<div id="redes" class="redes">
+				<div class="piecito"><br>
 				<div id="facebook">
-					<img src="img/facebook.png">
+					<a href="https://www.facebook.com/CDMEbenezer/">
+					<img src="img/facebook.png" width="35px" width="35px">
+					</a>
 				</div>
 				<div id="instagram">
-					<img src="img/instagram.png">
+					<img src="img/instagram.png" width="35px" width="35px">
 				</div>
+
 				<div id="youtube">
-					<img src="img/youtube.png">
+				<a href="https://www.youtube.com/channel/UCPbfa-rcoDYAauSutm9R_Xw">
+					<img src="img/youtube.png" width="35px" width="35px">
+				</a>
 				</div>
 				<div id="twitter">
-					<img src="img/twitter.png">
+					<img src="img/twitter.png" width="35px" width="35px">
+				</div>
 				</div>
 			</div>
 		<!-- Finaliza bloque de redes sociales -->
 		<footer>
-			<div id="footer">
+			<div id="footer" class="footer">
 				<p>2019-2020 © Casa de Misericordia Ministerios Ebenezer Puerto Barrios | TODOS LOS DERECHOS RESERVADOS</p>
+				<br>
 			</div>
 		</footer>
 	<!-- Finaliza bloque de footer -->
